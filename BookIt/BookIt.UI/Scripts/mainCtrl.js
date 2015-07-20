7af2982c8ef8f188b2fbccd5c2f78ac26d119db9
Рудь Денис Андреевич <Denis_Rud@epam.com>
2015-07-20 15:16:55 +0300

    Configuring UI project. Moving changes from branch to develop.

--- BookIt/BookIt.UI/Scripts/mainCtrl.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    'use strict';
-    var controllerId = 'mainCtrl';
-    angular.module('app').controller(controllerId,
-        ['bookingService', mainCtrl]);
-    function mainCtrl(bookingService) {
-
-    }
-})();
---
+++ BookIt/BookIt.UI/Scripts/master-page.js
@@ -0,0 +1,22 @@
+﻿var bookItApp = angular.module('bookItApp', ['ngRoute', 'bookItControllers']);
+
+bookItApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/reservations', {
+                templateUrl: 'html-partials/reservations.html',
+                controller: 'ReservationListCtrl'
+            }).
+            when('/reservations/:reservationId', {
+                templateUrl: 'html-partials/reservation-detail.html',
+                controller: 'ReservationDetailCtrl'
+            }).
+             when('/reservations-search', {
+                 templateUrl: 'html-partials/reservations-search.html',
+                 controller: 'ReservationSearchCtrl'
+             }).
+            otherwise({
+                redirectTo: '/reservations'
+            });
+    }]);
+
