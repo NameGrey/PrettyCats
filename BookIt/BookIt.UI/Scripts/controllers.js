6b876de7baab812b4d18d3fb068d5f578dad079d
Рудь Денис Андреевич <Denis_Rud@epam.com>
2015-07-23 13:32:27 +0300

    Other option to get list of objects via ajax

--- BookIt/BookIt.UI/Scripts/controllers.js
+++ BookIt/BookIt.UI/Scripts/controllers.js
@@ -1,21 +1,29 @@
 ﻿var bookItControllers = angular.module("bookItControllers", []);
 
-bookItControllers.controller('ReservationListCtrl', ['$scope', '$routeParams',
-  function ($scope) {
 
-      var uri = '/api/BookingEntities/GetBookingEntities';
-      getReservations();
+bookItControllers.controller('ReservationListCtrl',
+  function ($scope, $http) {
 
-      function getReservations() {
-              // Send an AJAX request
-              $.getJSON(uri)
-                  .done(function(data) {
-                      // On success, 'data' contains a list of products.
-                      $scope.details = data;
-                  alert('vavavava');
-              });
-          };
-  }]);
+      $http.get('http://localhost:55060/api/Booking/subjects').success(function(data, status, headers, config) {
+          $scope.details = data;
+      }).
+      error(function (data, status, headers, config) {
+         alert('Panic!!! Panic!!!');
+      });
+
+
+     // $scope.getReservations = function (){
+     //         // Send an AJAX request
+     //         $.getJSON($scope.uri)
+     //             .done(function(data) {
+     //                 // On success, 'data' contains a list of products.
+     //                 $scope.details = data;
+     //             alert('vavavava');
+     //         });
+     //};
+     //$scope.getReservations();
+
+  });
 
 bookItControllers.controller('ReservationDetailCtrl', ['$scope', '$routeParams',
   function ($scope) {
