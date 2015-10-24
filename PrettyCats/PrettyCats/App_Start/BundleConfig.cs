51ed45a04d1949155414061e61bb842985ed0895
Sergey <Sergey_timokhin@epam.com>
2015-10-24 23:07:56 +0300

    Index page was implemented.
    There was implemented the main page layout.

--- PrettyCats/PrettyCats/App_Start/BundleConfig.cs
+++ PrettyCats/PrettyCats/App_Start/BundleConfig.cs
@@ -7,23 +7,22 @@ namespace PrettyCats
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-1.9.1.js",
-						"~/Scripts/jquery-1.9.1.min.js"));
+						"~/Scripts/jquery-2.1.4.js",
+						"~/Scripts/jquery-2.1.4.min.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
+					  "~/Scripts/respond.js",
+					  "~/Scripts/viewportChecker.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css",
-					  "~/Content/SlideShow.css"));
-
-			bundles.Add(new StyleBundle("~/Content/js").Include(
-					  "~/Content/SlideShow.js"));
+					  "~/Content/business-frontpage.css",
+					  "~/Content/animate.css"));
 
 			BundleTable.EnableOptimizations = true;
 		}
