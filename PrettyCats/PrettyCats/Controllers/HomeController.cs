4a2b6f563fb1444876c27fe632d25a51c7f4d8a5
Sergey <Sergey_timokhin@epam.com>
2016-01-07 12:23:30 +0300

    Catch and log exceptions for any controllers

--- PrettyCats/PrettyCats/Controllers/HomeController.cs
+++ PrettyCats/PrettyCats/Controllers/HomeController.cs
@@ -2,11 +2,23 @@
 using System.Web;
 using System.Web.Mvc;
 using PrettyCats.Database;
+using PrettyCats.Helpers;
 
 namespace PrettyCats.Controllers
 {
 	public class HomeController : Controller
 	{
+		protected override void OnException(ExceptionContext filterContext)
+		{
+			LogHelper.WriteLog(Server.MapPath("~/App_Data/" + Settings.LogFileName), filterContext.Exception.ToString());
+
+			if (filterContext.HttpContext.IsCustomErrorEnabled)
+			{
+				filterContext.ExceptionHandled = true;
+				this.View("Error").ExecuteResult(this.ControllerContext);
+			}
+		}
+
 		// GET: Home
 		public ActionResult Index()
 		{
--- PrettyCats/PrettyCats/Controllers/InformPagesController.cs
+++ PrettyCats/PrettyCats/Controllers/InformPagesController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PrettyCats.Helpers;
 
 namespace PrettyCats.Controllers
 {
 	public class InformPagesController : Controller
 	{
+		protected override void OnException(ExceptionContext filterContext)
+		{
+			LogHelper.WriteLog(Server.MapPath("~/App_Data/" + Settings.LogFileName), filterContext.Exception.ToString());
+
+			if (filterContext.HttpContext.IsCustomErrorEnabled)
+			{
+				filterContext.ExceptionHandled = true;
+				this.View("Error").ExecuteResult(this.ControllerContext);
+			}
+		}
+
 		// GET: InformPages
 
 		public ActionResult HowChooseKitten()
