ce5b8ccb0d663c4c2a6a43c522607159516a7e59
Sergey <Sergey_timokhin@epam.com>
2016-01-21 23:37:56 +0300

    Add the possibility to handle 404 errors

--- PrettyCats/PrettyCats/Global.asax.cs
+++ PrettyCats/PrettyCats/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using PrettyCats.Controllers;
 
 namespace PrettyCats
 {
@@ -9,10 +12,26 @@ namespace PrettyCats
 	{
 		protected void Application_Start()
 		{
+			
 			log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/Web.config")));
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected void Application_EndRequest()
+		{
+			if (Context.Response.StatusCode == 404)
+			{
+				Response.Clear();
+
+				var rd = new RouteData();
+				rd.Values["controller"] = "Home";
+				rd.Values["action"] = "Index";
+
+				IController c = new HomeController();
+				c.Execute(new RequestContext(new HttpContextWrapper(Context), rd));
+			}
 		}
 	}
 }
