75faa8fa9480461ac9d85d0744f64fd106508cc8
Sergey <Sergey_timokhin@epam.com>
2016-02-13 23:37:28 +0300

    Add views for new category - articles. But it's not using yet.

---
+++ PrettyCats/PrettyCats/Models/ArticleModelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrettyCats.Models
+{
+	public class ArticleModelView
+	{
+		public string ImageUrl { get; set;}
+		public string ArticleName { get; set; }
+		public string MainArticleText { get; set; }
+		public string ArticleUrl { get; set; }
+
+		public ArticleModelView(string imageUrl, string articleName, string mainArticleText, string articleUrl)
+		{
+			this.ArticleName = articleName;
+			this.ImageUrl = imageUrl;
+			this.MainArticleText = mainArticleText;
+			this.ArticleUrl = articleUrl;
+		}
+	}
+}
