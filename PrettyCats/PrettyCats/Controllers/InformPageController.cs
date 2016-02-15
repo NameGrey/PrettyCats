886a12ea09ad23ccea4454e8915268dbf121dbb4
Sergey <Sergey_timokhin@epam.com>
2016-02-15 23:21:17 +0300

    Add the possibility to show articles on the site.

--- PrettyCats/PrettyCats/Controllers/InformPageController.cs
+++ PrettyCats/PrettyCats/Controllers/InformPageController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using PrettyCats.Database;
 using PrettyCats.Helpers;
 
@@ -33,7 +29,23 @@ namespace PrettyCats.Controllers
 			return View("InformPage", page);
 		}
 
+		[Route("kitten-cost-price")]
+		public ActionResult KittenCostArticle()
+		{
+			Pages page = new Pages();
+			page.Content = "sd";
+			page.Name = "Сколько стоят породистые котята?";
+
+			return View("KittenCostArticle", page);
+		}
+
 		public ActionResult Documents()
+		{
+			return View();
+		}
+
+		[Route("articles")]
+		public ActionResult Articles()
 		{
 			return View();
 		}
