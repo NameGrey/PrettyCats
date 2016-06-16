8173009d3f27749615d153a3d010d784068ece31
sergey Timokhin <sergey_timokhin@epam.com>
2016-06-16 17:10:41 +0300

    Extract work with pictures into separate JS file.

---
+++ PrettyCats/PrettyCats/Scripts/workWithPictures.js
@@ -0,0 +1,35 @@
+﻿function successRequest(itemId) {
+	$('#' + itemId).hide();
+}
+
+function successChangeOrderRequest(itemId, newOrder) {
+	$('input').find('#' + itemId).val(newOrder);
+}
+
+function ChangeOrder() {
+
+	var mas = [];
+	$('.kitten-block-admin').each(
+		function (i, entry) {
+			var id = $(entry).attr('id');
+			var order = $(entry).find('#' + id).val();
+
+			mas.push({ ID: id, Order: order });
+		});
+
+	//json += '}';
+	json = JSON.stringify(mas, null, 2);
+	//var json = $.toJSON(mas);
+
+	$.ajax({
+		url: '/Admin/ChangePicturesOrder',
+		type: 'POST',
+		datetype: 'json',
+		data: json,
+		contentType: 'application/json;',
+		success: function () {
+			$(':input').val('');
+			location.reload();
+		}
+	});
+}
