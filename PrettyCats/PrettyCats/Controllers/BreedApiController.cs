c4fa6e6c4a53d9c23ebf6f5e02517247f556b87e
sergey Timokhin <sergey_timokhin@epam.com>
2016-06-23 20:12:14 +0300

    Add a new one api controller: Breed Api controller

---
+++ PrettyCats/PrettyCats/Controllers/BreedApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using PrettyCats.DAL;
+using PrettyCats.DAL.Entities;
+using PrettyCats.DAL.Repositories;
+using PrettyCats.DAL.Repositories.DbRepositories;
+
+namespace PrettyCats.Controllers
+{
+	[RoutePrefix("api/breeds")]
+	public class BreedApiController:ApiController
+	{
+		private readonly IKittenBreedRepository _breedRepository;
+
+		public BreedApiController()
+		{
+			StorageContext context = new StorageContext();
+
+			_breedRepository = new DbBreedsRepository(context);
+		}
+
+		[Route("/")]
+		public IEnumerable<PetBreeds> Get()
+		{
+			return _breedRepository.GetCollection();
+		}
+	}
+}
