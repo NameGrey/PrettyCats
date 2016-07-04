0ea1a80c756423c9cf7003d646922378ddf1fefd
sergey Timokhin <sergey_timokhin@epam.com>
2016-07-04 14:57:38 +0300

    Fix links for the kittens

--- PrettyCats/PrettyCats.UI/Scripts/angular/directives.js
+++ PrettyCats/PrettyCats.UI/Scripts/angular/directives.js
@@ -9,7 +9,7 @@ artDuviksApp.directive("thumbnail", function() {
 
 			$scope.kitten.Link = kittensPathBuilder.buildKittenLink($scope.kitten);
 			kittensImageWorker.getKittenMainPicture($scope.kitten).success(function(data) {
-				$scope.kitten.Image = data;
+				$scope.kitten.MainPicture = data;
 			});
 		}
 }
