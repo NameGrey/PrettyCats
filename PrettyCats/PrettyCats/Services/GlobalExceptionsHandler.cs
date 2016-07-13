34b1b47bdd8c153df4875eb765f4ad5e895fadea
sergey Timokhin <sergey_timokhin@epam.com>
2016-07-13 10:10:43 +0300

    Add logging of global exceptions using log4net.

--- PrettyCats/PrettyCats/Services/GlobalExceptionsHandler.cs
+++ PrettyCats/PrettyCats/Services/GlobalExceptionsHandler.cs
@@ -1,12 +1,21 @@
 ﻿using System.Web.Http.ExceptionHandling;
+using log4net;
+using log4net.Config;
 
 namespace PrettyCats.Services
 {
 	public class GlobalExceptionsHandler:ExceptionHandler
 	{
+		private readonly ILog _logger = log4net.LogManager.GetLogger(typeof (GlobalExceptionsHandler));
+
+		public GlobalExceptionsHandler()
+		{
+			XmlConfigurator.Configure();
+		}
+
 		public override void Handle(ExceptionHandlerContext context)
 		{
-			//TODO: Add logging of exceptions
+			_logger.Error(context.Request.RequestUri, context.Exception);
 			base.Handle(context);
 		}
 	}
