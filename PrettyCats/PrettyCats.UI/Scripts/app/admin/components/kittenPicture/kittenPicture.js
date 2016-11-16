0d4c220d3df31bc8632be5d1cabdd53748b102b6
Timokhin, Sergey <Sergey.Timokhin@Kronos.com>
2016-11-16 16:45:57 +0300

    Add usage of drag and drop to sort kittens pictures

--- PrettyCats/PrettyCats.UI/Scripts/app/admin/components/kittenPicture/kittenPicture.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('AdminModule').component('kittenPicture', {
-    bindings: {
-        picture: "<"
-    },
-    templateUrl: "Scripts/app/admin/components/kittenPicture/kittenPicture.html"
-});
