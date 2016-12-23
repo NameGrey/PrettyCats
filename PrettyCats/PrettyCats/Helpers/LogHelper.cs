67546e2a5a0c5d3835e9466a69862350cccb1c81
Timokhin, Sergey <Sergey.Timokhin@Kronos.com>
2016-12-23 20:45:54 +0300

    Add TODO for logging system

--- PrettyCats/PrettyCats/Helpers/LogHelper.cs
+++ PrettyCats/PrettyCats/Helpers/LogHelper.cs
@@ -2,6 +2,9 @@
 
 namespace PrettyCats.Helpers
 {
+	//TODO: change logging system - use NLog instead of direct writing into file system
+	//TODO: and database using log4net. NLog is faster and logs async way. It doesn't affect
+	//TODO: perfomance of main application. It's needed to create a configuration inside application (not in conf file)
 	public static class LogHelper
 	{
 		public static void WriteLog(string filename, string message)
